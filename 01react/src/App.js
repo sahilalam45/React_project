508eb3a24486e15f8f79abfff3b4990e04387d40
Sahil Alam <sahilalamalam45@gamil.com>
2024-10-09 12:17:45 +0530

    Basics

---
+++ 01react/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from './chai'
+
+function App() {
+  return (
+    <>
+     <h1>Starting with React</h1>
+     <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01reactWithVite/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import Chai from './Chai'
+
+function App() {
+
+  return (
+    <>
+      <h1>React With Vite | Sahil Alam</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
